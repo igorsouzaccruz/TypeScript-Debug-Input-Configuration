0dfc40196f8f02671d1f834009f57cab156f19c1
Caíque Cavalcante Campanaro <105387453+caiquecampanaro@users.noreply.github.com>
2022-09-14 15:35:10 -0300

    inicial comit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { GerenciadorDeAlgoritimo } from './gerenciador_de_algoritimo';
 import { IAlgoritimo } from './interface/algoritimo';
+import { QuestaoUm } from './questao1/questao1';
 import { QuestaoDois } from './questao2/questao2';
 
 const algoritimos: Array<IAlgoritimo> = [new QuestaoDois()];
