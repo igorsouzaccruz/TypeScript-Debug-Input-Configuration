5aa123138024717653b2137e7d66f995b86de632
Ryan Fernandes <ryanfrnnds@gmail.com>
2022-09-15 20:31:12 -0300

    refatoracao

--- src/questao_8/questao8.ts
+++ src/questao_8/questao8.ts
@@ -7,7 +7,7 @@ export class QuestaoOito implements IAlgoritimo {
   a: number;
   b: number;
   entreAeB: number;
-  resultado: string;
+  resultado: string = '';
   numerosPrimos: number[] = [];
 
   //   Número primo é aquele que só é divisível por ele mesmo e pela unidade. Fazer um
@@ -19,13 +19,25 @@ export class QuestaoOito implements IAlgoritimo {
   }
 
   entradaDeDados(): void {
-    this.a = Number(this.prompt(' Informe o valor inicial: '));
-    this.b = Number(this.prompt(' Informe o valor final: '));
+    this.a = Number(
+      this.prompt(' Informe um número natural e positivo como valor inicial: ')
+    );
+    this.b = Number(
+      this.prompt(' Informe um número natural e positivo como valor final: ')
+    );
   }
 
   processamentoDosDados() {
-    if (this.b > this.a) {
+    if (this.a < 0 || this.b < 0) {
+      this.resultado += 'Nenhum número deve ser manor que 0(zero).';
+      return;
+    }
+    const bEhMaiorQueA = this.b > this.a;
+    if (bEhMaiorQueA) {
       for (let entreAeB = this.a; entreAeB <= this.b; entreAeB++) {
+        if (entreAeB === 1) {
+          continue;
+        }
         let ehPrimo = true;
         for (let divisor = 2; divisor < entreAeB; divisor++) {
           if (entreAeB % divisor == 0) {
@@ -37,9 +49,22 @@ export class QuestaoOito implements IAlgoritimo {
           this.numerosPrimos.push(entreAeB);
         }
       }
+      this.resultado += `Os primos entre ${this.a} e ${this.b} `;
+      const nenhumPrimoEncontrado = this.numerosPrimos.length === 0;
+      if (nenhumPrimoEncontrado) {
+        this.resultado += 'não existem';
+        return;
+      }
+      const variosPrimosEncontrados = this.numerosPrimos.length > 1;
+      this.resultado += ` ${variosPrimosEncontrados ? 'são' : 'é'} ${
+        this.numerosPrimos
+      }`;
+    } else {
+      this.resultado += 'O número inicial não deve ser maior que o final.';
     }
   }
+
   saidaDosDados() {
-    console.log(`O número ${this.numerosPrimos} `);
+    console.log(this.resultado);
   }
 }
