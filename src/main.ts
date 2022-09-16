a78895f7eb3b172209327624748cbf15d5fd7bae
Caíque Cavalcante Campanaro <105387453+caiquecampanaro@users.noreply.github.com>
2022-09-16 18:53:09 -0300

    Conclusão questao 7

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { GerenciadorDeAlgoritimo } from './gerenciador_de_algoritimo';
 import { IAlgoritimo } from './interface/algoritimo';
 import { QuestaoUm } from './questao1/questao1';
 import { QuestaoDois } from './questao2/questao2';
+import { QuestaoSete } from './questao7/questao7';
 
-const algoritimos: Array<IAlgoritimo> = [new QuestaoDois()];
+const algoritimos: Array<IAlgoritimo> = [new QuestaoSete()]
 
 const gerenciadorDosAlgoritimos: GerenciadorDeAlgoritimo =
   new GerenciadorDeAlgoritimo(algoritimos);
