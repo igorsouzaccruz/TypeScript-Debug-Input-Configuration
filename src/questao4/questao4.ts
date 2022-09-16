862c27a00af485cffe2c15cedc9d208cbbc7649f
Caíque Cavalcante Campanaro <105387453+caiquecampanaro@users.noreply.github.com>
2022-09-16 08:40:02 -0300

    adicionado a questão 4

--- src/questao4/questao4.ts
+++ src/questao4/questao4.ts
@@ -2,21 +2,23 @@ import PromptSync from 'prompt-sync'
 import { IAlgoritimo } from '../interface/algoritimo'
 export class QuestaoQuatro implements IAlgoritimo {
   prompt = PromptSync()
+  private somaNumeros: number = 0
 
   titulo(): string {
     return 'QUESTÃO 4'
   }
 
   entradaDeDados(): void {
-    
+    console.log('Não precisa de interação com o usuário')
   }
 
   processamentoDosDados() {
-
+    for (let numero = 0; numero < 100; numero++) {
+      this.somaNumeros += numero + 1
+    }
   }
 
-
   saidaDosDados() {
-
+    console.log(`A soma dos números inteiros de 1 á 100 é: ${this.somaNumeros}`)
   }
 }
