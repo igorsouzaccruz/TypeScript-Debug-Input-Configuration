832f4ba1bcbb1cdb0a656bfdafba3afd7aa51fbc
Caíque Cavalcante Campanaro <105387453+caiquecampanaro@users.noreply.github.com>
2022-09-16 14:00:13 -0300

    Foi adiciona a questão 6

--- src/questao6/questao6.ts
+++ src/questao6/questao6.ts
@@ -2,21 +2,26 @@ import PromptSync from 'prompt-sync'
 import { IAlgoritimo } from '../interface/algoritimo'
 export class QuestaoSeis implements IAlgoritimo {
   prompt = PromptSync()
- 
+  temperaturaFahrenheit: number
+  temperaturaCelsius: number
 
   titulo(): string {
     return 'QUESTÃO 6'
   }
 
   entradaDeDados(): void {
-  
+    this.temperaturaFahrenheit = Number(
+      this.prompt('Informe a temperatura em Fahrenheit: ')
+    )
   }
 
   processamentoDosDados() {
-
+    this.temperaturaCelsius = (5 * (this.temperaturaFahrenheit - 32)) / 9
   }
 
   saidaDosDados() {
-
+    console.log(
+      `A temperatura em Fahrenheit é ${this.temperaturaFahrenheit} e a mesma temperatura em Celsius é ${this.temperaturaCelsius} `
+    )
   }
 }
