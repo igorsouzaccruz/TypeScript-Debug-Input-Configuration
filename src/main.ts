06444f46ac45fc5917a5358ed31641728ee8c1e7
Ryan Fernandes <ryanfrnnds@gmail.com>
2022-09-17 12:56:14 -0300

    Questoes de 1 a 4 MAIN

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,13 @@ import { QuestaoUm } from './questao1/questao1';
 import { QuestaoDois } from './questao2/questao2';
 import { QuestaoTres } from './questao3/questao3';
 import { QuestaoQuatro } from './questao4/questao4';
-import { QuestaoCinco } from './questao5/questao5';
-import { QuestaoSeis } from './questao6/questao6';
-import { QuestaoSete } from './questao7/questao7';
-import { QuestaoOito } from './questao8/questao8';
-import { QuestaoNove } from './questao9/questao9';
-import { QuestaoDez } from './questao10/questao10'
-import { QuestaoOnze } from './questao11/questao11'
-import { QuestaoDoze } from './questao12/questao12'
-import { QuestaoTreze } from './questao13/questao13';
-import { QuestaoQuatorze } from './questao14/questao14';
 
-const algoritimos: Array<IAlgoritimo> = [new QuestaoQuatorze()]
+const algoritimos: Array<IAlgoritimo> = [
+  new QuestaoUm(),
+  new QuestaoDois(),
+  new QuestaoTres(),
+  new QuestaoQuatro(),
+];
 
 const gerenciadorDosAlgoritimos: GerenciadorDeAlgoritimo =
   new GerenciadorDeAlgoritimo(algoritimos);
